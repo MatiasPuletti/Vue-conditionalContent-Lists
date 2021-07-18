cf298b0ca012f0691e8cc3d58fe9ec8d837437b1
matiaspuletti <pulettimatias@gmail.com>
2021-07-18 18:22:24 +0200

    Add logic so that when clicking on the add task buttonn a new task with the entered value is added

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredTaskValue: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.enteredTaskValue);
+    }
+  }
+});
+
+app.mount('#assignment');
