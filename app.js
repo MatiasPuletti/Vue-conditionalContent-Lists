0043bd190f72d746790d6b75a5ac593fcb7f5953
matiaspuletti <pulettimatias@gmail.com>
2021-07-18 18:30:04 +0200

    Create toggle button to hide or show the list upon being clicked

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       enteredTaskValue: '',
-      tasks: []
+      tasks: [],
+      taskListIsVisible: true
     };
   },
   methods: {
     addTask() {
       this.tasks.push(this.enteredTaskValue);
+    },
+    toggleTaskList() {
+      this.taskListIsVisible = !this.taskListIsVisible;
     }
   }
 });
